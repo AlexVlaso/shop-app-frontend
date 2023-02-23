ab3cdff15791b4d4fcf416210dbf487185c336a4
AlexVlaso <alexvlas.javaprog@gmail.com>
2023-02-23 11:41:16 +0200

    add methods addProduct... to CartService

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,11 +1,30 @@
 import { Injectable } from '@angular/core';
+import { CartItem } from '../model/cart-item';
 import { Product } from '../model/product';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
-  cart: Product[] = [];
-  selectedProduct: Product[] = [];
+  cart: CartItem[] = [];
+  selectedProduct: CartItem[] = [];
   constructor() {}
+
+  addProductToCart(product: Product) {
+    this.addProductToList(product, this.cart);
+  }
+  addProductToSelected(product: Product) {
+    this.addProductToList(product, this.selectedProduct);
+  }
+  private addProductToList(product: Product, list: CartItem[]) {
+    const curCartItem = new CartItem(product);
+    const existingCartItem = list.find(
+      (tempCartItem) => tempCartItem.product.id === curCartItem.product.id
+    );
+    if (existingCartItem) {
+      existingCartItem.quantity += 1;
+    } else {
+      list.push(curCartItem);
+    }
+  }
 }
