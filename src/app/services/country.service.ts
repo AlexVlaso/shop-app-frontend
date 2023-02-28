249d9ddcf679fa447e0d478b347e586acd849225
AlexVlaso <alexvlas.javaprog@gmail.com>
2023-02-28 12:37:08 +0200

    add country service

--- src/app/services/country.service.ts
+++ src/app/services/country.service.ts
@@ -1,12 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { State } from '@popperjs/core';
+import { Observable } from 'rxjs';
+import { Country } from '../model/country';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CountryService {
-  countryUrl = 'http://localhost:8080/api/countries';
+  countriesUrl = 'http://localhost:8080/api/countries';
+  statesUrl = 'http://localhost:8080/api/states';
   constructor(private httpClient: HttpClient) {}
 
-  getListOfCountries() {}
+  getListOfCountries(): Observable<countryResponse> {
+    return this.httpClient.get<countryResponse>(this.countriesUrl);
+  }
+  getListOfStates(code: string): Observable<stateResponse> {
+    return this.httpClient.get<stateResponse>(
+      `${this.statesUrl}/search/findStatesByCountryCode?code=${code}`
+    );
+  }
+}
+interface countryResponse {
+  _embedded: {
+    counties: Country[];
+  };
+}
+interface stateResponse {
+  _embedded: {
+    states: State[];
+  };
 }
