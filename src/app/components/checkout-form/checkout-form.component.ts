4d6d92a9684b59cb145fda76de33f5b50db23627
AlexVlaso <alexvlas.javaprog@gmail.com>
2023-03-01 13:08:28 +0200

    add validation to checkout form

--- src/app/components/checkout-form/checkout-form.component.ts
+++ src/app/components/checkout-form/checkout-form.component.ts
@@ -4,6 +4,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Country } from 'src/app/model/country';
 import { State } from 'src/app/model/state';
 import { CountryService } from 'src/app/services/country.service';
+import { ShopValidator } from 'src/app/validators/shopValidator';
 
 @Component({
   selector: 'app-checkout-form',
@@ -21,16 +22,36 @@ export class CheckoutFormComponent implements OnInit {
   ngOnInit(): void {
     this.checkoutFormsGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName: [''],
-        lastName: [''],
-        email: [''],
+        firstName: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(2),
+            ShopValidator.notOnlyWhiteSpace,
+          ],
+        ],
+        lastName: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(2),
+            ShopValidator.notOnlyWhiteSpace,
+          ],
+        ],
+        email: [
+          '',
+          [
+            Validators.required,
+            Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+          ],
+        ],
       }),
       shippingAddress: this.formBuilder.group({
-        country: [''],
-        region: [''],
-        city: [''],
-        address: [''],
-        zipCode: [''],
+        country: ['', Validators.required],
+        region: ['', Validators.required],
+        city: ['', [Validators.required, ShopValidator.notOnlyWhiteSpace]],
+        address: ['', [Validators.required, ShopValidator.notOnlyWhiteSpace]],
+        zipCode: ['', [Validators.required, ShopValidator.notOnlyWhiteSpace]],
       }),
       creditCard: this.formBuilder.group({
         card: [''],
@@ -49,6 +70,22 @@ export class CheckoutFormComponent implements OnInit {
   get email() {
     return this.checkoutFormsGroup.get('customer.email');
   }
+  get country() {
+    return this.checkoutFormsGroup.get('shippingAddress.country');
+  }
+  get region() {
+    return this.checkoutFormsGroup.get('shippingAddress.region');
+  }
+  get city() {
+    return this.checkoutFormsGroup.get('shippingAddress.city');
+  }
+  get address() {
+    return this.checkoutFormsGroup.get('shippingAddress.address');
+  }
+  get zipCode() {
+    return this.checkoutFormsGroup.get('shippingAddress.zipCode');
+  }
+
   handleListOfStates() {
     const formGroup = this.checkoutFormsGroup.get('shippingAddress');
     const countryCode = formGroup?.value.country.code;
@@ -58,6 +95,7 @@ export class CheckoutFormComponent implements OnInit {
     });
   }
   onSubmit() {
+    console.log(this.firstName?.errors);
     console.log(this.checkoutFormsGroup.get('customer')?.value);
     console.log(this.checkoutFormsGroup.get('shippingAddress')?.value);
   }
