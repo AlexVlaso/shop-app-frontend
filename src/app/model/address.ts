88d590e9df5282899f6e6075fb90766791aff36b
AlexVlaso <alexvlas.javaprog@gmail.com>
2023-03-02 14:55:22 +0200

    add class address

---
+++ src/app/model/address.ts
@@ -0,0 +1,9 @@
+export class Address {
+  constructor(
+    public country: string,
+    public state: string,
+    public city: string,
+    public address: string,
+    public zipCode: string
+  ) {}
+}
